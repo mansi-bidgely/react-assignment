aabe93775e002644de7ea2ede89d1937fd131231
Mansi Jain <mansijain3113@gmail.com>
2020-07-07 13:35:12 +0530

    add all folders

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import DashboardIndex from './components/DashboardIndex.jsx'
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <DashboardIndex />
+    </div>
+  );
+}
+
+export default App;
