f7464a913d5bc80fc9f54f6909a7e3e33d83c3fd
Mansi Jain <mansijain3113@gmail.com>
2020-07-07 15:34:13 +0530

    push review comments

--- src/helpers/apiData.js
+++ src/helpers/apiData.js
@@ -1,35 +1,38 @@
 export const stateClickData = () => {
-  return fetch("https://api.covid19india.org/data.json")
-  .then(results => {
-  return results.json();
-  })
-}
+  return fetch("https://api.covid19india.org/data.json").then((results) => {
+    return results.json();
+  });
+};
 
-export const DefaultStateData = () => {
-  const DEFAULT_STATE = "Madhya Pradesh"
-  return fetch( "http://covid19-india-adhikansh.herokuapp.com/state/" + DEFAULT_STATE)
-  .then(results => {
-  return results.json();
-  })
-}
-  
-export const TableDataRender = () => {
-  return fetch( "https://api.covidindiatracker.com/state_data.json")
-  .then(results => {
-  return results.json();
-  })
-}
+export const defaultStateData = () => {
+  const DEFAULT_STATE = "Madhya Pradesh";
+  return fetch(
+    "http://covid19-india-adhikansh.herokuapp.com/state/" + DEFAULT_STATE
+  ).then((results) => {
+    return results.json();
+  });
+};
 
-export const HandleKeyData = (stateSearch) => {
-  return fetch( 'http://covid19-india-adhikansh.herokuapp.com/state/'+ stateSearch)
-  .then(results => {
-  return results.json();
-  })
-}
+export const tableDataRender = () => {
+  return fetch("https://api.covidindiatracker.com/state_data.json").then(
+    (results) => {
+      return results.json();
+    }
+  );
+};
 
-export const HandleClickData = (stateSearch) => {
-  return fetch( 'http://covid19-india-adhikansh.herokuapp.com/state/'+ stateSearch)
-  .then(results => {
-  return results.json();
-  })
-}
+export const handleKeyData = (stateSearch) => {
+  return fetch(
+    "http://covid19-india-adhikansh.herokuapp.com/state/" + stateSearch
+  ).then((results) => {
+    return results.json();
+  });
+};
+
+export const handleClickData = (stateSearch) => {
+  return fetch(
+    "http://covid19-india-adhikansh.herokuapp.com/state/" + stateSearch
+  ).then((results) => {
+    return results.json();
+  });
+};
